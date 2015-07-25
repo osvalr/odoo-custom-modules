45719b3d16ba7e42e4f65329889bd066a0509c40
osval <osval@vauxoo.com>
2015-07-25 18:23:44 -0430

    [ADD] openacademy: new module for training purposes

---
+++ openacademy/__openerp__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Open Academy",
+
+    'summary': """Manage Trainings""",
+
+    'description': """
+        Open Academy module for managing trainings:
+            - training courses
+            - training sessions
+            - attendees registration
+    """,
+
+    'author': "osvalr",
+
+    'website': "http://www.lacarcaza.com.ve",
+
+    'category': 'OpenAcademy',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+
+    ],
+
+    'demo': [
+
+    ],
+    'installable': True,
+    'auto_install': False,
+}
