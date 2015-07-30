e08da205239cd06b12764f8ba1a0fc4fc06c1e6e
osval <osval@vauxoo.com>
2015-07-30 16:30:10 -0430

    [REF] openacademy: added computed fields and onchanged decorators

--- openacademy/model/openacademy_session.py
+++ openacademy/model/openacademy_session.py
@@ -1,4 +1,4 @@
-from openerp import api, fields, models
+from openerp import _, api, fields, models
 
 """
     OpenAcademy session module
@@ -9,9 +9,14 @@ class Session(models.Model):
     _name = "openacademy.session"
 
     name = fields.Char(required=True)
-    start_date = fields.Date()
+
+    start_date = fields.Date(default=fields.Date.today)
+
     duration = fields.Float(digits=(6, 2), help= "Duration in Days")
+
     seats = fields.Integer(string="Number of seats")
+
+    active = fields.Boolean(default=True)
 
     instructor_id = fields.Many2one('res.partner', string="Instructor",
         domain=['|',
@@ -32,3 +37,20 @@ class Session(models.Model):
             self.taken_seats = .0
         else:
             self.taken_seats = 100.0 * len(self.attendee_ids)/self.seats
+
+    @api.onchange('seats', 'attendee_ids')
+    def _verify_valid_seats(self):
+        if self.seats < 0 :
+            return {
+                'warning':{
+                    'title' : _("Incorrect 'seats' value"),
+                    'message' : _("The number of available seats may not be negative"),
+                }
+            }
+        if self.seats < len(self.attendee_ids):
+            return {
+                'warning' : {
+                    'title' : _("Too many attendess"),
+                    'message' : _("Increase seats or remove excess attendees"),
+                }
+            }
