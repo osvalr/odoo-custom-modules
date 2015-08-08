47d8d176a73b6a08b204f42a9967a13070cc98b0
osval <osval@vauxoo.com>
2015-08-07 20:10:30 -0430

    [REF] openacademy: do changes

---
+++ openacademy/tests/openacademy_course.py
@@ -0,0 +1,12 @@
+from openerp.tests.common import TransactionCase
+
+class TestCourse(TransactionCase):
+    """
+    Test for OpenAcademy Course
+    """
+
+    def setUp(self):
+        super(TestCourse, self).setUp()
+
+
+
